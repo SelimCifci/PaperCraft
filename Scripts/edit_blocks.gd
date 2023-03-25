79a86254d59cbce5ddc4693c86de75b3d6e78477
SelimCifci <cifcihasanselim@gmail.com>
2023-03-25 15:15:29 -0700

    Fixed placing blocks inside of entities

--- Scripts/edit_blocks.gd
+++ Scripts/edit_blocks.gd
@@ -1,27 +1,34 @@
 extends Node2D
 
 onready var tilemap = $Terrain
-export var raycast_collision = Vector2.ZERO
+export var raycast_collision_break = Vector2.ZERO
+export var raycast_collision_build = Vector2.ZERO
 
 var entity_in = false
 
 func _physics_process(_delta):
-	raycast_collision = tilemap.world_to_map($Player/RayCast2D.get_collision_point() - $Player/RayCast2D.get_collision_normal() * 8)
+	var collision_point = $Player/RayCast2D.get_collision_point()
+	var collision_normal = $Player/RayCast2D.get_collision_normal()
 	
-	var tile = tilemap.get_cellv(raycast_collision)
+	raycast_collision_break = tilemap.world_to_map(collision_point - collision_normal * 8)
+	raycast_collision_build = raycast_collision_break + collision_normal
+	
+	$CheckEntity.position = tilemap.map_to_world(raycast_collision_build)
+	
+	var tile = tilemap.get_cellv(raycast_collision_break)
 	var selected_item = $Player/Hotbar.items[$Player/Hotbar.selected_slot]
 	
-	if Input.is_action_just_pressed("build") and can_place(raycast_collision):
-		tilemap.set_cellv(raycast_collision + $Player/RayCast2D.get_collision_normal(), selected_item[0])
+	if Input.is_action_just_pressed("build") and can_place(raycast_collision_build):
+		tilemap.set_cellv(raycast_collision_build, selected_item[0])
 		selected_item[1] -= 1
 	elif Input.is_action_just_pressed("destroy") and tile != TileMap.INVALID_CELL and tile != 3:
 		give_item(tile)
-		tilemap.set_cellv(raycast_collision, TileMap.INVALID_CELL)
+		tilemap.set_cellv(raycast_collision_break, TileMap.INVALID_CELL)
 			
 func can_place(pos):
 	var can_place = false
 
-	if $Player/Hotbar.items[$Player/Hotbar.selected_slot][1] >= 1 and tilemap.get_cellv(pos + $Player/RayCast2D.get_collision_normal()) == TileMap.INVALID_CELL:
+	if $Player/Hotbar.items[$Player/Hotbar.selected_slot][1] >= 1 and tilemap.get_cellv(pos) == TileMap.INVALID_CELL:
 		can_place = true
 		
 	if entity_in:
@@ -39,3 +46,13 @@ func give_item(tile):
 			item[0] = tile
 			item[1] += 1
 			break
+
+func _on_CheckEntity_body_entered(body):
+	if body.get("name") != "Terrain":
+		entity_in = true
+		print("in")
+
+func _on_CheckEntity_body_exited(body):
+	if body.get("name") != "Terrain":
+		entity_in = false
+		print("out")
